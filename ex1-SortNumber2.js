fdc30bcaa00eb5b1e2958afcd07b61f5d70e7aa5
Iliana Slavcheva <124202194+Ilianata@users.noreply.github.com>
2023-03-03 18:59:31 +0200

    Update ex1-SortNumber2.js

--- ex1-SortNumber2.js
+++ ex1-SortNumber2.js
@@ -6,24 +6,27 @@ function sort2(first, second, third) {
   } else if (third > first && third > second) {
     console.log(third);
   }
-  if ((first > second && first < third) || (first < second && first > third)) {
+  if (
+    (first >= second && first <= third) ||
+    (first <= second && first >= third)
+  ) {
     console.log(first);
   } else if (
-    (second > first && second < third) ||
-    (second < first && second > third)
+    (second >= first && second <= third) ||
+    (second <= first && second >= third)
   ) {
     console.log(second);
   } else if (
-    (third > first && third < second) ||
-    (third < first && third > second)
+    (third >= first && third <= second) ||
+    (third <= first && third >= second)
   ) {
     console.log(third);
   }
-  if (first < second && first < third) {
+  if (first <= second && first <= third) {
     console.log(first);
-  } else if (second < first && second < third) {
+  } else if (second <= first && second <= third) {
     console.log(second);
-  } else if (third < first && third < second) {
+  } else if (third <= first && third <= second) {
     console.log(third);
   }
 }
